1c9baea25b64e1198d96096f3f3b173080531c4f
Nicholas Roberts <nicholas@browserstack.com>
2022-10-27 14:28:08 -0700

    OCR 10

--- playwright-test/fixtures.js
+++ playwright-test/fixtures.js
@@ -13,8 +13,8 @@ const caps = {
   browser: 'chrome',
   os: 'osx',
   os_version: 'catalina',
-  name: 'Nicholas Browserstack Inception POC OCR 9 Github Actions',
-  build: 'nicholas-playwright-inception-build-2022-POC-OCR-9 Github Actions',
+  name: 'Nicholas Browserstack Inception POC OCR 10 Github Actions',
+  build: 'nicholas-playwright-inception-build-2022-POC-OCR-10 Github Actions',
   'browserstack.username': process.env.BROWSERSTACK_USERNAME || 'YOUR_USERNAME',
   'browserstack.accessKey':
     process.env.BROWSERSTACK_ACCESS_KEY || 'YOUR_ACCESS_KEY',
